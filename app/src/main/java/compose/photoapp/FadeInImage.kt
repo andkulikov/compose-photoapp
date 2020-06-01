0c6b7ddc2c68459b91af9f50d1ce0356c25ad707
Andrey Kulikov <andreykulikov@google.com>
2020-06-01 12:25:44 +0100

    Rearrange FadeInImage 2

--- app/src/main/java/compose/photoapp/FadeInImage.kt
+++ app/src/main/java/compose/photoapp/FadeInImage.kt
@@ -20,13 +20,10 @@ fun FadeInImage(id: Int, modifier: Modifier = Modifier)   {
     } else {
         val alpha = animatedFloat(0f)
         onCommit(image) {
-            alpha.snapTo(0f)
-            if (image != null) {
-                alpha.animateTo(1f, TweenBuilder<Float>().apply {
-                    duration = 300
-                    easing = LinearOutSlowInEasing
-                })
-            }
+            alpha.animateTo(1f, TweenBuilder<Float>().apply {
+                duration = 300
+                easing = LinearOutSlowInEasing
+            })
         }
         Image(
             image,
