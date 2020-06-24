65e468a825a9b6e0d10227437ae895f76d5f62c9
Andrey Kulikov <andreykulikov@google.com>
2020-06-25 00:36:17 +0100

    Add PhotoAppTheme preview

--- app/src/main/java/compose/photoapp/PhotoAppTheme.kt
+++ app/src/main/java/compose/photoapp/PhotoAppTheme.kt
@@ -1,11 +1,14 @@
 package compose.photoapp
 
 import androidx.compose.Composable
+import androidx.ui.core.Modifier
+import androidx.ui.foundation.Text
 import androidx.ui.foundation.isSystemInDarkTheme
 import androidx.ui.graphics.Color
-import androidx.ui.material.MaterialTheme
-import androidx.ui.material.darkColorPalette
-import androidx.ui.material.lightColorPalette
+import androidx.ui.layout.padding
+import androidx.ui.material.*
+import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 
 @Composable
 fun PhotoAppTheme(content: @Composable() () -> Unit) {
@@ -19,4 +22,19 @@ fun PhotoAppTheme(content: @Composable() () -> Unit) {
     )
     val colors = if (isSystemInDarkTheme()) darkColors else lightColors
     MaterialTheme(colors = colors, content = content)
+}
+
+@Preview
+@Composable
+fun PhotoAppThemePreview() {
+    PhotoAppTheme {
+        Surface {
+            Button(
+                onClick = {},
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text("Demo")
+            }
+        }
+    }
 }
